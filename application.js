092ec865e7b9a548bf3eda3e38fd70ee883cb0c6
Adam Back <adb33@email.arizona.edu>
2014-08-04 14:12:15 -0700

    Document now loads tweets via script on index.html when ready instead of application.js

--- application.js
+++ application.js
@@ -1,25 +1,6 @@
-var loadTweets = function() {
-  //var now = moment();
-  var $div = $('.display');
-
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div id="message"></div>');
-      var $label = $('<button id="label"></button>');
-      var $time = $('<div id="time"></div>');
-      $label.text('@' + tweet.user + ':').appendTo($div);
-      $tweet.text(tweet.message).appendTo($div);
-      $time.text(tweet.created_at.toLocaleTimeString()).appendTo($div);
-      index -= 1;
-    }
-};
-
 $(document).ready(function(){
-  loadTweets(); 
 
   $('#refresh').on('click', function() {
-    $('div').html('');
     loadTweets();
   });
 
