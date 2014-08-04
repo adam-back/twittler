57cf2e3ca4643b7511254f084f851bb36d003e4e
Adam Back <adb33@email.arizona.edu>
2014-08-04 11:31:52 -0700

    Styled new time div and refactored appends.

--- application.js
+++ application.js
@@ -7,11 +7,9 @@ var loadTweets = function() {
       var tweet = streams.home[index];
       var $tweet = $('<div id="message"></div>');
       var $label = $('<div id="label"></div>');
-      var $time = $('<div> id ="time"></div>');
+      var $time = $('<div id="time"></div>');
       $label.text('@' + tweet.user + ':').appendTo($div);
-      $label.appendTo($div);
-      $tweet.text(tweet.message);
-      $tweet.appendTo($div);
+      $tweet.text(tweet.message).appendTo($div);
       $time.text(tweet.created_at.toLocaleTimeString()).appendTo($div);
       index -= 1;
     }
